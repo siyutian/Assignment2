58e8739acccad187902870e335dadf0de2c0df9a
siyu tian <siyutianstevens@gmail.com>
2013-08-19 17:35:51 -0400

    aaa
    aaa

--- src/p1/MySet.java
+++ src/p1/MySet.java
@@ -7,7 +7,14 @@ public class MySet<V> {
 	private ArrayList<V> elements;
 	private ArrayList<Integer> hashCodes;
 	
-	public bool
+	public boolean add(V element){
+		Iterator<V> it=elements.iterator();
+		while(it.hasNext()){
+			if(element.equals(it.next()))
+				return false;
+		}
+		elements.add(element);
+		hashCodes.add(element.hashCode());
 		return true;
 		
 	}
